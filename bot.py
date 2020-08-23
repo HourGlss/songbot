6528d400b310611f9607e174e5f05b7ee9b3ebda
D2Check <misterfyxit@gmail.com>
2020-08-23 15:58:58 -0400

    don't spam the chat

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import os # for importing env vars for the bot to use
 from twitchio.ext import commands
 from winamp.winamp import Winamp
 wina = None
+import time
+last_spoke = None
 
 bot = commands.Bot(
     # set up the bot
@@ -14,8 +16,10 @@ bot = commands.Bot(
 
 @bot.event
 async def event_ready():
+    global last_spoke
     global wina
     wina = Winamp()
+    last_spoke = time.time() - 20
     'Called once when the bot goes online.'
     print(f"{os.environ['BOT_NICK']} is online!")
     ws = bot._ws  # this is only needed to send messages within event_ready
@@ -24,7 +28,11 @@ async def event_ready():
 
 @bot.command(name='song')
 async def song(ctx):
-    await ctx.send(f'{wina.getCurrentTrackName()}')
+    global last_spoke
+    now = time.time()
+    if now - last_spoke > 30:
+        last_spoke = now
+        await ctx.send(f'{wina.getCurrentTrackName()}')
 
 if __name__ == "__main__":
     bot.run()
