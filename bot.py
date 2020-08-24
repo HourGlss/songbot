3139d976e83c64f1fb6307e1ed0db5b9d2a708c0
D2Check <misterfyxit@gmail.com>
2020-08-24 17:27:17 -0400

    added readme

--- bot.py
+++ bot.py
@@ -1,17 +1,18 @@
 import os # for importing env vars for the bot to use
 from twitchio.ext import commands
 from winamp.winamp import Winamp
+import env as config
 wina = None
 import time
 last_spoke = None
 
 bot = commands.Bot(
     # set up the bot
-    irc_token=os.environ['TMI_TOKEN'],
-    client_id=os.environ['CLIENT_ID'],
-    nick=os.environ['BOT_NICK'],
-    prefix=os.environ['BOT_PREFIX'],
-    initial_channels=[os.environ['CHANNEL']]
+    irc_token=config['TMI_TOKEN'],
+    client_id=config['CLIENT_ID'],
+    nick=config['BOT_NICK'],
+    prefix=config['BOT_PREFIX'],
+    initial_channels=[config['CHANNEL']]
 )
 
 @bot.event
